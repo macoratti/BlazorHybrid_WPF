0bda1bdd4728410dbf21f9b57d391336ce783843
Macoratti .net <macoratti@yahoo.com>
2024-05-21 18:25:53 -0300

    Add project files.

---
+++ RazorLibrary/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary;
+
+public class Veiculo
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Modelo { get; set; }
+    public int Ano { get; set; }
+    public string? Imagem { get; set; }
+}
